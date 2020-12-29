52dc7a02d4a7f7d1ff569e0560f9e6fdc35ef4e5
Robson <robsoncezariosilva@gmail.com>
2020-12-29 00:49:50 -0300

    Move port to 4000

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 require("./database");
 
-app.listen(3000, () => {
-  console.log("Server listening at door: 3000");
+app.listen(4000, () => {
+  console.log("Server listening at port: 4000");
 });
